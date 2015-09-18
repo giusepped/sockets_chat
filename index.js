955149918258f85fa53b31687256ec697a915ae5
giuseppe de santis <giusdesan@gmail.com>
2015-09-18 09:58:21 +0100

    finished version 1

--- index.js
+++ index.js
@@ -1,11 +1,22 @@
 var app = require('express')();
 var http = require('http').Server(app);
 var express = require('express');
+var io = require('socket.io')(http);
 
 app.use(express.static(__dirname + "/public"));
 
 app.get('/', function(req, res){
   res.send(__dirname + '/index.html');
+});
+
+io.on('connection', function(socket) {
+  console.log('a user is connected');
+  socket.on('disconnect', function() {
+    console.log('user disconnected');
+  });
+  socket.on('chat message', function(msg){
+    io.emit('chat message', msg);
+  });
 });
 
 http.listen(3000, function(){
